719e5ae7509fa12c91967bbc7b584ddce44ae50a
NuevoNombre <nuevoemail@example.com>
2025-05-17 19:00:26 -0300

    Normalizando saltos de línea en yarn.lock

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-export function NavMenu({ onClickLink }) {
+export function NavMenu() {
   return (
     <nav className="nav-menu">
       <ul>
-        <li><a href="#" onClick={onClickLink}>Inicio</a></li>
-        <li><a href="#" onClick={onClickLink}>Nosotros</a></li>
-        <li><a href="#" onClick={onClickLink}>Cursos</a></li>
-        <li><a href="#" onClick={onClickLink}>Contacto</a></li>
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="nosotros.html">Nosotros</a></li>
+        <li><a href="cursos.html">Cursos</a></li>
+        <li><a href="contacto.html">Contacto</a></li>
       </ul>
     </nav>
   );
 }
+
